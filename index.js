894330d6045b101467e9bdd5a41353ecd24d00b3
Carlosressurreicao <carlosxavier1952@gmail.com>
2024-05-26 19:49:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 <body> <script src="https://cdn.faceio.net/fio.js"></script> <script src="./index.js"></script> </body>
+  enroll.addEventListener("click", async () => { let response = await faceio.enroll({ locale: "auto", payload: { email: "example@gmail.com", pin: "12345", }, }); console.log(` Unique Facial ID: ${response.facialId} Enrollment 
+  Date: ${response.timestamp} 
+  Gender: ${response.details.gender} 
+  Age Approximation: ${response.details.age}`); });
